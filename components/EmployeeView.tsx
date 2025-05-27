efebb79ecb25751c9b3b728849d9c18e7c83a0fc
Dennis Santiago <denniscsantiago@gmail.com>
2025-05-28 01:56:26 +0800

    Added peer display

--- components/EmployeeView.tsx
+++ components/EmployeeView.tsx
@@ -5,7 +5,7 @@ import { Text, TouchableOpacity, View } from 'react-native';
 const EmployeeView = ({selectedEmployee}) => {
     return (
         <>
-            <TouchableOpacity onPress={() => console.log('Employee pressed')}>
+            <TouchableOpacity onPress={() => console.log('Employee pressed', selectedEmployee)}>
                 <View key={selectedEmployee.details.id} style={{
                     backgroundColor: 'white',
                     borderColor: 'black',
@@ -65,6 +65,35 @@ const EmployeeView = ({selectedEmployee}) => {
                                 }}>
                                     <Text>{directReport.firstName} {directReport.lastName}</Text>
                                     <Text>{directReport.role}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        ))}
+                    </TouchableOpacity>
+                </View>
+            }
+
+            {
+                selectedEmployee.peers.length > 0 &&
+                <View style={{
+                    padding: 10,
+                }}>
+                    <Text>
+                        {selectedEmployee.details.firstName} {selectedEmployee.details.firstName} works with
+                    </Text>
+                    <TouchableOpacity onPress={() => console.log('get employee')}>
+                        {selectedEmployee.peers.map((peer) => (
+                            <TouchableOpacity key={peer.id} onPress={() => console.log('peer pressed')}>
+                                <View style={{
+                                    backgroundColor: 'white',
+                                    borderColor: 'black',
+                                    borderWidth: 1,
+                                    padding: 20,
+                                    margin: 10,
+                                    borderRadius: 10,
+                                    display: 'flex',
+                                }}>
+                                    <Text>{peer.firstName} {peer.lastName}</Text>
+                                    <Text>{peer.role}</Text>
                                 </View>
                             </TouchableOpacity>
                         ))}
