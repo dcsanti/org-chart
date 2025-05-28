d2dcd24b9633cb69113cd360f20a7755db8ee1f0
Dennis Santiago <denniscsantiago@gmail.com>
2025-05-29 00:01:16 +0800

    Renamed file

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -35,7 +35,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="add"
         options={{
           title: 'Add',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="plus.circle" color={color} />,
