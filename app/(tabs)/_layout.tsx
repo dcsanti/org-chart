0f6e2e6b86b0d4c2a1c2ff672504062814905d11
Dennis Santiago <denniscsantiago@gmail.com>
2025-05-28 12:12:01 +0800

    Icon change

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,6 +7,7 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -30,7 +31,7 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Org Chart',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="homekit" color={color} />,
+          tabBarIcon: ({ color }) => <SimpleLineIcons name="organization" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
