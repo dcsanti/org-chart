feb84226bf8ad0ae1054ff3ace7cd1cb5331d6f2
Dennis Santiago <denniscsantiago@gmail.com>
2025-05-29 02:03:29 +0800

    Added ability to create companies and employees, save them in the cache and submit a post request every 5 seconds

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -1,23 +1,333 @@
-import { StyleSheet, Text, View } from 'react-native';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+import { Modal, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function TabTwoScreen() {
+    const [company, setCompany] = useState('');
+    const [employee, setEmployee] = useState({
+        firstName: '',
+        lastName: '',
+        role: '',
+        reportsTo: '',
+        companyId: '',
+        address: {
+            streetNumber: '',
+            address1: '',
+            address2: '',
+            city: '',
+            country: 'NZ'
+        }
+    });
+    const [modalVisible, setModalVisible] = useState(false);
+    const countries = ['NZ', 'AU', 'UK'];
+
+    useEffect(() => {
+        getFromAsyncStorage('companies')
+            .then(existingCompanies => {
+                console.log("onLoad", existingCompanies);
+            });
+
+        getFromAsyncStorage('employees')
+            .then(existingCompanies => {
+                console.log("onLoad", existingCompanies);
+            });
+    }, [])
+
+    const storeInAsyncStorage = async (key, value) => {
+        try {
+            const jsonValue = JSON.stringify(value);
+            await AsyncStorage.setItem(key, jsonValue);
+        } catch (e) {
+            console.error("Error saving data to AsyncStorage", e);
+        }
+    };
+
+    const getFromAsyncStorage = async (key) => {
+        try {
+            const jsonValue = await AsyncStorage.getItem(key);
+            
+            return jsonValue != null ? JSON.parse(jsonValue) : null;
+        } catch (e) {
+            console.error("Error retrieving data from AsyncStorage", e);
+            return null;
+        }
+    };
+
+    const updateEmployee = (field, value) => {
+        if (field.includes('.')) {
+            const [parent, child] = field.split('.');
+            setEmployee(prev => ({
+                ...prev,
+                [parent]: {
+                    ...prev[parent],
+                    [child]: value
+                }
+            }));
+        } else {
+            setEmployee(prev => ({
+                ...prev,
+                [field]: value
+            }));
+        }
+    };
+
+    const handleAddCompany = () => {
+        const companyData = {
+            id: Date.now(),
+            name: company.trim()
+        }
+
+        console.log(companyData);
+        // get the existing companies from async storage and append the new company
+        getFromAsyncStorage('companies')
+            .then(existingCompanies => {
+                const companies = existingCompanies || [];
+                companies.push(companyData);
+                return storeInAsyncStorage('companies', companies);
+            })
+            .catch(error => {
+                console.error("Error retrieving or storing companies", error);
+            });
+
+
+        getFromAsyncStorage('employees')
+            .then(existingEmployees => {
+                console.log("Existing Companies:", existingEmployees);
+            });
+    };
+
+    const handleAddEmployee = () => {
+        const newEmployee = {
+            id: Date.now(),
+            firstName: employee.firstName.trim(),
+            lastName: employee.lastName.trim(),
+            role: employee.role.trim(),
+            reportsTo: employee.reportsTo ? employee.reportsTo.trim() : null, // should be a valid employee ID under the same companyID
+            companyId: employee.companyId ? employee.companyId.trim() : null, // should be a valid companyID
+            address: {
+                streetNumber: employee.address.streetNumber.trim(),
+                address1: employee.address.address1.trim(),
+                address2: employee.address.address2.trim(),
+                city: employee.address.city.trim(),
+                country: employee.address.country
+            }
+        };
+        console.log(newEmployee);
+
+        // get the existing employees from async storage and append the new employee
+        getFromAsyncStorage('employees')
+            .then(existingEmployees => {
+                const employees = existingEmployees || [];
+                employees.push(newEmployee);
+                return storeInAsyncStorage('employees', employees);
+            })
+            .catch(error => {
+                console.error("Error retrieving or storing employees", error);
+            });
+    }
+    
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
-            <Text>test</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#fff', marginBottom: 40 }}>
+            <ScrollView style={styles.container}>
+                <View style={styles.formSection}>
+                    <Text style={styles.title}>Add New Company</Text>
+                    <Text style={styles.label}>Company Name</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={company}
+                        onChangeText={setCompany}
+                        placeholder="Company Name"
+                        placeholderTextColor="#888"
+                    />
+                </View>
+                <Pressable 
+                    style={styles.submitButton} 
+                    onPress={handleAddCompany}
+                    disabled={!company.trim()}>
+                    <Text style={styles.buttonText}>Add Company</Text>
+                </Pressable>
+
+                <View style={styles.formSection}>
+                    <Text style={styles.sectionTitle}>Employee Details</Text>
+
+                    <Text style={styles.label}>Company</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={employee.companyId} // Assuming companyId is a string for simplicity
+                        onChangeText={(value) => updateEmployee('companyId', value)}
+                        placeholder="Company"
+                        placeholderTextColor="#888"
+                    />
+                    
+                    <Text style={styles.label}>Reports to</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={employee.reportsTo} // Assuming reportsTo is a string for simplicity
+                        onChangeText={(value) => updateEmployee('reportsTo', value)}
+                        placeholder="Reports to"
+                        placeholderTextColor="#888"
+                    />
+
+                    {['firstName', 'lastName', 'role'].map((field) => (
+                        <View key={field}>
+                            <Text style={styles.label}>{field.charAt(0).toUpperCase() + field.slice(1)}</Text>
+                            <TextInput
+                                style={styles.input}
+                                value={employee[field]}
+                                onChangeText={(value) => updateEmployee(field, value)}
+                                placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
+                                placeholderTextColor="#888"
+                            />
+                        </View>
+                    ))}
+                    
+                    <Text style={styles.label}>Address</Text>
+                    {['streetNumber', 'address1', 'address2', 'city'].map((field) => (
+                        <TextInput
+                            key={field}
+                            style={styles.input}
+                            value={employee.address[field]}
+                            onChangeText={(value) => updateEmployee(`address.${field}`, value)}
+                            placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
+                            placeholderTextColor="#888"
+                        />
+                    ))}
+                    
+                    <Text style={styles.label}>Country</Text>
+                    <Pressable 
+                        style={styles.dropdownButton}
+                        onPress={() => setModalVisible(true)}>
+                        <Text>{employee.address.country}</Text>
+                    </Pressable>
+                </View>
+                
+                <Pressable 
+                    style={styles.submitButton} 
+                    onPress={handleAddEmployee}
+                    disabled={!employee.companyId.trim() || !employee.reportsTo.trim() || !employee.firstName.trim() || !employee.lastName.trim()}>
+                    <Text style={styles.buttonText}>Add Employee</Text>
+                </Pressable>
+            </ScrollView>
+
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => setModalVisible(false)}>
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <Text style={styles.modalTitle}>Select Country</Text>
+                        {countries.map((country) => (
+                            <TouchableOpacity
+                                key={country}
+                                style={styles.countryItem}
+                                onPress={() => {
+                                    updateEmployee('address.country', country);
+                                    setModalVisible(false);
+                                }}>
+                                <Text style={styles.countryText}>{country}</Text>
+                            </TouchableOpacity>
+                        ))}
+                        <TouchableOpacity
+                            style={styles.cancelButton}
+                            onPress={() => setModalVisible(false)}>
+                            <Text style={styles.cancelButtonText}>Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+    formSection: {
+        marginBottom: 20,
+    },
+    label: {
+        fontSize: 16,
+        marginBottom: 5,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 4,
+        backgroundColor: '#fff',
+    },
+    submitButton: {
+        backgroundColor: 'teal',
+        padding: 15,
+        borderRadius: 8,
+        marginBottom: 40,
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    sectionTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    dropdownButton: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 10,
+        borderRadius: 4,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        width: '80%',
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        padding: 20,
+        alignItems: 'center',
+    },
+    modalTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    countryItem: {
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd',
+        width: '100%',
+        alignItems: 'center',
+    },
+    countryText: {
+        fontSize: 16,
+    },
+    cancelButton: {
+        marginTop: 20,
+        padding: 10,
+        backgroundColor: '#ff3b30',
+        borderRadius: 4,
+    },
+    cancelButtonText: {
+        color: '#fff',
+        fontSize: 16,
+    },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import Tree from "@/components/Tree";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
 import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import data from "../../data/data";
@@ -6,6 +7,27 @@ import data from "../../data/data";
 export default function HomeScreen() {
     const [orgData, setOrgData] = useState();
 
+    const getFromAsyncStorage = async (key) => {
+        try {
+            const jsonValue = await AsyncStorage.getItem(key);
+            
+            return jsonValue != null ? JSON.parse(jsonValue) : null;
+        } catch (e) {
+            console.error("Error retrieving data from AsyncStorage", e);
+            return null;
+        }
+    };
+
+    const storeInAsyncStorage = async (key, value) => {
+        try {
+            const jsonValue = JSON.stringify(value);
+            await AsyncStorage.setItem(key, jsonValue);
+        } catch (e) {
+            console.error("Error saving data to AsyncStorage", e);
+        }
+    };
+
+    // Fetch dummy data from local file so we can display some data in the tree view
     useEffect(() => {
         const fetchOrg = async (): Promise => {
             const orgData = await new Promise((resolve) =>
@@ -16,6 +38,78 @@ export default function HomeScreen() {
           };
 
         fetchOrg();
+    }, []);
+
+    useEffect(() => {
+        // every 5 seconds, query the async storage and post the data to an API endpoint
+        const interval = setInterval(() => {
+            const postCompaniesData = async () => {
+                const companies = await getFromAsyncStorage("companies");
+                if (!companies) {
+                    console.warn("No companies data found in AsyncStorage");
+                    return;
+                }
+
+                // get the first element of the companies array
+                const firstCompany = companies.shift();
+
+                // update the companies array in AsyncStorage
+                await storeInAsyncStorage('companies', companies);
+
+                try {
+                    const response = await fetch("https://example.com/api/endpoint", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(firstCompany), // 
+                    });
+
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+
+                    const data = await response.json();
+                    console.log("Data posted successfully:", data);
+                } catch (error) {
+                    console.error("Error posting data:", error);
+                }
+            };
+
+            const postEmployeesData = async () => {
+                const employees = await getFromAsyncStorage("employees");
+                if (!employees) {
+                    console.warn("No employees data found in AsyncStorage");
+                    return;
+                }
+
+                // get the first element of the employees array
+                const firstEmployee = employees.shift();
+
+                // update the employees array in AsyncStorage
+                await storeInAsyncStorage('employees', employees);
+                try {
+                    const response = await fetch("https://example.com/api/endpoint", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(firstEmployee), //
+                    });
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    const data = await response.json();
+                    console.log("Data posted successfully:", data);
+                } catch (error) {
+                    console.error("Error posting data:", error);
+                }
+            };
+
+            postEmployeesData();
+            postCompaniesData();
+        }, 5 * 1000); // trigger call every 5 seconds
+        return () => clearInterval(interval); // Cleanup on unmount
     }, []);
 
     return (
@@ -30,20 +124,20 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        height: 178,
+        width: 290,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
 });
